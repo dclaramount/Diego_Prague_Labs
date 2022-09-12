bf1103682c2643b3d4b758e9ecb7c70358e19cf2
dclaramount <diegoaclaramount@gmail.com>
2022-09-12 11:02:53 +0200

    Implement Token Authentication

--- DeveloperTest/Program.cs
+++ DeveloperTest/Program.cs
@@ -1,17 +1,88 @@
+using System.Reflection;
+using System.Text;
 using DeveloperTest;
+using DeveloperTest.DbContexts;
+using DeveloperTest.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+//  Configuring my Swagger UI
+//  Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+
+    setupAction.AddSecurityDefinition("DeveloperTestBearerAuth", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Input a valid token to access this API"
+    });
+
+    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "DeveloperTestBearerAuth" }
+            }, new List<string>() }
+    });
+});
+//To Map Entities -> DTOs and vice versa
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//Token Authnetication / Authorization Policy
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeUser", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("access_level", "User");
+    });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
 
-builder.Services.AddDbContext<ToDoDbContext>(configure =>
-    configure.UseInMemoryDatabase("ToDoDatabase")
-        .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)));
+}).AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+})
+  .AddXmlDataContractSerializerFormatters();
+builder.Services.AddDbContext<StoreContext>(dbContextOptions => dbContextOptions.UseInMemoryDatabase(databaseName: "Test_Diego")
+.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)));
+
+
+builder.Services.AddScoped<IStoreRepository, StoreRepository>();
 
 var app = builder.Build();
 
@@ -22,6 +93,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
